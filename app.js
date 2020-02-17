1dbc8c2c8b7314a3b86d21b379d976a34fabf8f7
LeoAuyeung <leo297747@gmail.com>
2020-02-17 06:11:40 -0500

    Add Part 1e - Use Node to Make HTTP Requests to Our API

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const Handlebars = require("handlebars"); // https://www.npmjs.com/package/handlebars
 const exphbs = require('express-handlebars') // https://www.npmjs.com/package/express-handlebars
-var fs = require('fs');
+const fs = require('fs');
+const axios = require('axios'); // https://www.npmjs.com/package/axios
 
 const app = express()
 app.use(express.urlencoded({ extended: false })); // necessary for HTML form submission to express
@@ -40,3 +41,160 @@ app.post('/submit', function (req, res) {
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// ============================== Part 1e) Axios functions ==============================
+
+function getUsers() {
+    return axios({
+        method: 'get',
+        url: 'https://hunter-todo-api.herokuapp.com/user',
+        // params: {
+        //     username: 'bobthebuilder'
+        // }
+    })
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+function addUser(username) {
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/user',
+        data: {
+            username: username,
+        }
+    })
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+function authUser(username) {
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/auth',
+        data: {
+            username: username,
+        }
+    })
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+function getToDoList(username) {
+    // first login and get token
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/auth',
+        data: {
+            username: username,
+        }
+    })
+    .then((response) => axios({
+        method: 'get',
+        url: 'https://hunter-todo-api.herokuapp.com/todo-item',
+        headers: {
+            Authorization: response.data.token,
+        }
+    }))
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error.response);
+    })
+}
+
+function addToDo(username, item) {
+    // first login and get token
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/auth',
+        data: {
+            username: username,
+        }
+    })
+    .then((response) => axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/todo-item',
+        headers: {
+            Authorization: response.data.token,
+            'content-type': 'application/json',
+        },
+        data: {
+            "content": item
+        }
+    }))
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error.response);
+    })
+}
+
+function updateToDo(username, id) {
+    // first login and get token
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/auth',
+        data: {
+            username: username,
+        }
+    })
+    .then((response) => axios({
+        method: 'put',
+        url: `https://hunter-todo-api.herokuapp.com/todo-item/${id}`,
+        headers: {
+            Authorization: response.data.token,
+            'content-type': 'application/json',
+        },
+        data: {
+            "completed": true
+        }
+    }))
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error.response);
+    })
+}
+
+function deleteToDo(username, id) {
+    // first login and get token
+    return axios({
+        method: 'post',
+        url: 'https://hunter-todo-api.herokuapp.com/auth',
+        data: {
+            username: username,
+        }
+    })
+    .then((response) => axios({
+        method: 'delete',
+        url: `https://hunter-todo-api.herokuapp.com/todo-item/${id}`,
+        headers: {
+            Authorization: response.data.token,
+        },
+    }))
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error.response);
+    })
+}
+
+// ============================== Part 1e) Axios functions END ==============================
+
+// (Main) calling axios functions
+
+// getUsers()
+// addUser('bobthebuilder');
+// authUser('bobthebuilder')
+getToDoList('bobthebuilder')
+// addToDo('bobthebuilder', 'building123')
+// updateToDo('bobthebuilder', 189)
+// deleteToDo('bobthebuilder', 190)
